3918a994ca7592bfed1887e9dd4cc82042e3daba
cesarcalvo171-cyber <cesarcalvo171@gmail.com>
2026-07-22 10:55:14 -0600

    chore: connect to new supabase database instance

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fldcsbnxgrsihyltljhh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZGNzYm54Z3JzaWh5bHRsamhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNTU2NjUsImV4cCI6MjA5NzczMTY2NX0.Dmm0VdP5TnqOqR-U7DOyGm1lmWDJ8JSYsfCorzhoOZQ';
+const supabaseUrl = 'https://bjfzjbwrrmqnlmfbxmeg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqZnpqYndycm1xbmxtZmJ4bWVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MzAzMTIsImV4cCI6MjEwMDMwNjMxMn0.p1zomWoXLSLPCUlD_0u9RoKUnHFXma1Cpx2JsyjkHAg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
